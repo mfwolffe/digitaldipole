946b91219bb7e5b0e03621b1b973f24db18ac2cb
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-03-31 15:42:23 -0400

    initial take on calc modeling

--- config/settings/base.py
+++ config/settings/base.py
@@ -90,6 +90,7 @@ LOCAL_APPS = [
     "dipole.users",
     # Your stuff: custom apps go here
     "dipole.reference",
+    "dipole.calculators"
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
