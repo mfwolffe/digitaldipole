c69c84f161a9515507c38df283465a911bf662d0
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-05-18 16:55:25 -0400

    ability to jump to elements + warning modal for bad query

--- dipole/frontend/src/pages/tabulated.jsx
+++ dipole/frontend/src/pages/tabulated.jsx
@@ -6,6 +6,7 @@ import Card from "react-bootstrap/Card";
 import Tabs from "react-bootstrap/Tabs";
 import Form from 'react-bootstrap/Form';
 import Table from 'react-bootstrap/Table';
+import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Popover from 'react-bootstrap/Popover';
 import Carousel from 'react-bootstrap/Carousel';
@@ -22,6 +23,8 @@ import { all } from '@awesome.me/kit-a655910996/icons'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+const CircleX = <FontAwesomeIcon className="pr-3" icon="fa-duotone fa-circle-x" style={{"--fa-primary-color": "#e22828", "--fa-secondary-color": "#e22828",}} />;
+const CircleCheck = <FontAwesomeIcon className="pr-3" icon="fa-duotone fa-circle-check" style={{"--fa-primary-color": "#69d510", "--fa-secondary-color": "#69d510",}} />;
 library.add(...all);
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -79,12 +82,11 @@ function AtomTable(atomData) {
     return rows;
   }
 
-  // TODO maybe just have the table header be the color for the PT group
   return (
       <Table className="w-80 ml-auto mr-auto mt-2 carousel-table b-shadow" striped hover style={{"backgroundColor": colorTable[atomData['GroupBlock']]}}>
         <thead>
           <tr>
-            <th colSpan={2}>{ atomData['Name'] }</th>
+            <th colSpan={2} style={{"backgroundColor": colorTable[atomData['GroupBlock']], "color": "white",}}>{ atomData['Name'] }</th>
           </tr>
           <tr>
             <th className="text-left pl-4">Property</th>
@@ -113,24 +115,65 @@ const CarouselTip = (
 
 const Tabulated = () => {
     const atoms = buildAtomJSON();
+
+    const [show, setShow]   = useState(false);
     const [index, setIndex] = useState(0);
+
+    const handleShow = () => {
+      setShow(true);
+    }
+
+    const handleClose = () => {
+      setShow(false);
+    }
 
     const handleSelect = (selectedIndex) => {
         setIndex(selectedIndex);
     };
 
-    console.log(data);
-
     const slides = []
-
     atoms.forEach((atom) => {
       slides.push(AtomSlide(atom));
     })
 
+    function jumpTo (eventKey) {
+      eventKey.preventDefault();
+
+      const query = eventKey.target[0].value;
+      let n = 0;
+
+      if (!isNaN((n = Number(query)))) {
+        n = Math.floor(n);
+
+        if (n < 1 || n > 118) {
+          handleShow();
+          return;
+        }
+        setIndex(n - 1);
+
+        return;
+      }
+
+      const found = atoms.find((atom) => isMatch(atom));
+
+      if (!found) {
+        handleShow()
+        console.log("No match for user query");
+        return;
+      }
+
+      const foundIndex = found.AtomicNumber - 1;
+      setIndex(foundIndex);
+
+      function isMatch(element) {
+        return element.Name.toLowerCase() === query.toLowerCase() || element.Symbol === query;
+      }
+    }
+
     return (
         <div className="landing-container mt-3">
             <div className="landing mt-0">
-                <Card id="ref-default" className="mt-5 m-auto ">
+                <Card id="ref-default" className="mt-5 m-auto ref-card">
                   <Tabs
                   defaultActiveKey="AtomCarousel"
                   id="uncontrolled-tab-example"
@@ -139,26 +182,81 @@ const Tabulated = () => {
                       eventKey="AtomCarousel"
                       className="calc-tab"
                       title="Atom Carousel">
-                        <div className="d-flex justify-content-between w-99 m-auto">
-                          <p className="lead pl-2">Atom Carousel</p>
-                          <Form inline>
+                        <div className="d-flex justify-content-between align-items-baseline w-99 m-auto">
+                            <p className="lead pl-2 mb-0">Atom Carousel</p>
+                          <Form inline onSubmit={(e) => jumpTo(e)}>
                             <InputGroup>
                                 <OverlayTrigger trigger="focus" overlay={CarouselTip} placement="bottom" delay="2000">
                                     <Form.Control type="text" placeholder="Jump-to" id="jump-to"/>
                                 </OverlayTrigger>
-                                <Button className="sbar-btn hvr-grow-rotate">
+                                <Button type="submit" className="sbar-btn hvr-grow-rotate" >
                                     <FontAwesomeIcon icon="fa-duotone fa-magnifying-glass" fontSize={"1.4rem"}/>
                                 </Button>
                             </InputGroup>
                           </Form>
                         </div>
 
-                        <Card className="bg-transparent brdr-none ml-auto mr-auto mt-2 w-80 h-100">
+                        <Card className="bg-transparent brdr-none ml-auto mr-auto mt-2 w-95 h-100">
                           <Carousel activeIndex={index} fade interval={null} onSelect={handleSelect} className="m-auto w-100">
                             { slides }
                           </Carousel>
                         </Card>
-
+                        <Modal                           
+                          show={show}
+                          onHide={handleClose}
+                          backdrop="static"
+                          keyboard={false}>
+                          <Modal.Header closeButton className="modal-bg">
+                            <Modal.Title>Query found no match</Modal.Title>
+                          </Modal.Header>
+                          <Modal.Body className="modal-bg w-100">
+                            <p>Queries must either be an atom's name in US English, atomic number, or symbol on the periodic table</p>
+                            <p>Examples:</p>
+                            <div className="d-flex">
+                              <Table className="w-70 m-auto">
+                                <thead>
+                                  <tr>
+                                    <th scope="col">Query</th>
+                                    <th scope="col" class="text-right">Validity</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <td><em>hydrogen</em></td>
+                                    <td class="text-right">{ CircleCheck }</td>
+                                  </tr>
+                                  <tr>
+                                    <td><em>sulphur</em></td>
+                                    <td class="text-right">{ CircleX }</td>
+                                  </tr>
+                                  <tr>
+                                    <td><em>81</em></td>
+                                    <td class="text-right">{ CircleCheck }</td>
+                                  </tr>
+                                  <tr>
+                                    <td><em>128</em></td>
+                                    <td class="text-right">{ CircleX }</td>
+                                  </tr>
+                                  <tr>
+                                    <td><em>Ti</em></td>
+                                    <td class="text-right">{ CircleCheck }</td>
+                                  </tr>
+                                  <tr>
+                                    <td><em>Zz</em></td>
+                                    <td class="text-right">{ CircleX }</td>
+                                  </tr>
+                                </tbody>
+                              </Table>
+                            </div>
+                          </Modal.Body>
+                          <Modal.Footer className="modal-bg d-flex flex-row justify-content-end align-items-start">
+                            <Button
+                              className="mb-2 mt-2 dp-button"
+                              onClick={handleClose}>
+                              Back
+                            </Button>
+                          </Modal.Footer>
+                        </Modal>
                     </Tab>
                   </Tabs>
                 </Card>
